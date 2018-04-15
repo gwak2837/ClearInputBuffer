f3f32e8a8a2480f50fb063569e6a0f0f4eb0289e
곽태욱 <33145190+rmfpdlxmtidl@users.noreply.github.com>
2018-04-15 17:06:07 +0900

    Update clearInputBuffer.c

--- clearInputBuffer.c
+++ clearInputBuffer.c
@@ -1,3 +1,6 @@
+// Available with ASCII
+// Get string from user(stdio), save in pointer to char, and clear input buffer.
+
 void clearInputBuffer(char* string, int sizeOfAlloc)
 {
 	int length = strlen(string);
@@ -5,4 +8,17 @@ void clearInputBuffer(char* string, int sizeOfAlloc)
 		string[length] = '\0';
 	else
 		while (getchar() != '\n');
+}
+
+//-------------------Example-------------------
+
+#define EQUATION_SIZE 10 // Length of string
+
+int main()
+{
+	char* equation = (char*)malloc(EQUATION_SIZE);
+	fgets(equation, EQUATION_SIZE, stdin);
+	clearInputBuffer(equation, EQUATION_SIZE);
+  
+	return 0;
 }
